428e095a2ffcb632fd05fdb514687d09c7819aa2
Livio121212 <livio.reding@student.unisg.ch>
2024-11-03 11:50:44 +0100

    asd

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
 # Funktion, um den WG-Namen zu speichern
-def get_wg_name():
-    if 'wg_name' not in st.session_state:
-        st.session_state['wg_name'] = None
+def name_sharedflat():
+    if "SF_name" not in st.session_state:
+        st.session_state["SF_name"] = None
 
-    if st.session_state['wg_name'] is None:
-        st.session_state['wg_name'] = st.text_input("Gib den Namen deiner WG ein:", key="wg_input")
-        if st.session_state['wg_name']:
-            st.success(f"WG-Name '{st.session_state['wg_name']}' wurde gespeichert.")
+    if st.session_state["SF_name"] is None:
+        st.session_state["SF_name"] = st.text_input("Enter the name of your shared flat:", key="wg_input")
+        if st.session_state["SF_name"]:
+            st.success(f"Shared flat '{st.session_state['SF_name']}' wurde gespeichert.")
 
 # App-Layout
 st.title("Waistless")
 
 # Initiale Eingabe des WG-Namens
-get_wg_name()
+name_sharedflat()
 
 # Hauptinhalt der App
-if st.session_state['wg_name']:
-    st.write(f"Willkommen in der Waistless-App für die WG '{st.session_state['wg_name']}'!")
+if st.session_state["SF_name"]:
+    st.write(f"Willkommen in der Waistless-App für die WG '{st.session_state['SF_name']}'!")
 else:
-    st.warning("Bitte gib einen WG-Namen ein, um fortzufahren.")
+    st.warning("Please enter a flat-share name to continue.")
