93135242665d6a222c229f37dffb7e5eb52e35b9
Livio121212 <livio.reding@student.unisg.ch>
2024-11-04 11:36:04 +0100

    livio

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import streamlit as st
 
+# open first the setting page to define flate-name and inhabitants
+if "pages" not in st.session_state:
+    st.session_state ["page"] = "settings"
+
+# Function to change between the pages
+def change_page(new_page):
+    st.session_state["page"] = new_page
+
 
 # sidebar with selectboxes
-page= st.sidebar.selectbox("overiew","fridge", "recips", "settings")
+st.sidebar.title("Navigation")
+st.sidebar.selectbox("overview",st.sidebar.button("overview", on_click=lambda: change_page("overview")) )
+st.sidebar.button("fridge", on_click=lambda: change_page('fridge'))
+st.sidebar.button("recipes", on_click=lambda: change_page("recipes"))
+st.sidebar.button("settings", on_click=lambda: change_page("settings"))
 
 
-# Funktion für die Eingabeseite des Flat-Namens
-def flat_name_input():
-    st.title("♻️ Wasteless App - Setup")
 
-    # Eingabefeld für den Flat-Namen
-    flat_name = st.text_input("Enter your flat name:")
+# Anzeigelogik für jede Seite
+if st.session_state["page"] == "overview":
+    st.title("overview")
+    st.write('Willkommen auf der Startseite deiner App.')
+    st.write('Hier kannst du allgemeine Informationen anzeigen.')
+elif st.session_state["page"] == "fridge":
+    st.title("fridge")
+    st.write('Dies ist der Inhalt von Seite 1.')
+    st.text_input('Gib deinen Namen ein:', key='name_input_page1')
+    st.button('Bestätigen')
+elif st.session_state["page"] == "recipes":
+    st.title("recipes")
+    st.write('Dies ist der Inhalt von Seite 2.')
+    st.slider('Wähle einen Wert:', 0, 100, 50, key='slider_page2')
+elif st.session_state["page"] == "settings":
+    st.title("settings")
+    st.write('Dies ist der Inhalt von Seite 1.')
+    st.text_input('Gib deinen Namen ein:', key='name_input_page1')
+    st.button('Bestätigen')
 
-    # Button zur Bestätigung des Flat-Namens
-    if st.button("Approve Flat Name"):
-        if flat_name:
-            st.session_state.flat_name = flat_name  # Speichert den Namen in der Session
-            st.session_state.page = "add_participants"  # Wechselt zur nächsten Seite
-            st.experimental_rerun()
-        else:
-            st.warning("Please enter a flat name.")
 
 
