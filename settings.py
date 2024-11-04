ad241a8d9a738b3235f8b6ec2d2af1ebef023c08
Livio121212 <livio.reding@student.unisg.ch>
2024-11-04 14:21:07 +0100

    ii

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 # Initialisiere flate_name in st.session_state
-st.session_state["flate_name"] = ""
+if "flate_name" not in st.session_state:
+    st.session_state["flate_name"] = ""
 
 
 
@@ -9,10 +10,12 @@ st.session_state["flate_name"] = ""
 if st.session_state["flate_name"] == "":
     st.title("🏠 Wasteless App - Setup")
     flate_name = st.text_input("Please enter your flat name")
-    st.session_state.flate_name =flate_name
     if st.button("Confirm"):
-        st.text_input (f"welcome to your new flat {flate_name}. Please enter your name", key=room_mate)
+        st.session_state.flate_name =flate_name
+        st.text_input (f"welcome to your new flat {flate_name}. Please enter your name", key= "room_mate")
         st.button ("Add a new roommate")
+        st.experimental_rerun()
+        
 else:
     st.write ("livio best")
 
