655b6e9faf2ec75cb8726b940243510ef554ed56
Livio121212 <livio.reding@student.unisg.ch>
2024-11-05 11:24:36 +0100

    Delete Fridge_page.py

--- Fridge_page.py
+++
@@ -1,5 +0,0 @@
-def fridge_page():
-    st.title("Fridge")
-    st.write("This is the content of the Fridge page.")
-    st.text_input("Enter your name:", key="name_input_fridge")
-    st.button("Confirm")
