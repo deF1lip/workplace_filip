d42c6f7d00f82978849fdb842fcbb02bd3fde730
Livio121212 <livio.reding@student.unisg.ch>
2024-11-06 19:48:03 +0100

    kljö

--- workplaceblea_recipe.py
+++ workplaceblea_recipe.py
@@ -3,4 +3,5 @@ import streamlit as st
 # intiatlisierung 
 if "inventory" not in st.session_state:
     st.session_state["inventory"] = {"Tomato", "Banana,"} # Example roommates list
-#vw
+if "roommates" not in st.session_state:
+    st.session_state["roommates"] = ["Livio", "Flurin", "Anderin"]  # Example roommates list
