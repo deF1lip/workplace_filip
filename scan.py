c77ebc893025918105addbdcd129d792f696e53a
Livio121212 <livio.reding@student.unisg.ch>
2024-11-06 09:39:57 +0100

    sf

--- scan.py
+++ scan.py
@@ -1,1 +1,10 @@
-upload stuff
+import streamlit as st
+
+# Initialization of session state variables
+if "roommates" not in st.session_state:
+    st.session_state["roommates"] = ["Livio", "Flurin", "Anderin"]  # Example roommates list
+
+
+# Upload-Widget für die Rechnung
+uploaded_file = st.file_uploader("Upload an image of your receipt", type=["jpg", "jpeg", "png"])
+
