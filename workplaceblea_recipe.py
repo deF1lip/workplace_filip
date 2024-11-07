c9b24563dc81381c7c478b86bce7616d9d2c0ebd
BeeeliB <bela.wyss@student.unisg.ch>
2024-11-07 09:40:30 +0100

    changes made test

--- workplaceblea_recipe.py
+++ workplaceblea_recipe.py
@@ -5,3 +5,4 @@ if "inventory" not in st.session_state:
     st.session_state["inventory"] = {"Tomato", "Banana,"} # Example roommates list
 if "roommates" not in st.session_state:
     st.session_state["roommates"] = ["Livio", "Flurin", "Anderin"]  # Example roommates list
+# test 
