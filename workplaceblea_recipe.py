08cdd3173e6e7c3e2ed54629cadd304da748be74
BeeeliB <bela.wyss@student.unisg.ch>
2024-11-07 11:15:20 +0100

    scheiss idee livio

--- workplaceblea_recipe.py
+++ workplaceblea_recipe.py
@@ -12,10 +12,10 @@ if "selected_user" not in st.session_state:
 def select_user():
     st.sidebar.title("Who are you")
     if st.session_state["roommates"]:
-        selected_user = st.sidebar.selectbox("Choose your name:", st.session_state["roommates"])
+        selected_user = st.selectbox("Choose your name:", st.session_state["roommates"])
         st.session_state["selected_user"] = selected_user
-        st.sidebar.write(f"Hi, {selected_user}!")
+        st.selectbox.write(f"Hi, {selected_user}!")
     else:
-        st.sidebar.warning("No user was added.")
+        st.selectbox.warning("No user was added.")
 
 select_user()
