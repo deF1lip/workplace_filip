7c0efca14f8bdb0716d844d8615fd2426b9cf170
BeeeliB <bela.wyss@student.unisg.ch>
2024-11-07 10:29:53 +0100

    code für roommates

--- workplaceblea_recipe.py
+++ workplaceblea_recipe.py
@@ -5,4 +5,17 @@ if "inventory" not in st.session_state:
     st.session_state["inventory"] = {"Tomato", "Banana,"} # Example roommates list
 if "roommates" not in st.session_state:
     st.session_state["roommates"] = ["Livio", "Flurin", "Anderin"]  # Example roommates list
-# test 
+
+# Variabel für ausgewählter Mitbewohner
+if "selected_user" not in st.session_state:
+    st.session_state["selected_user"] = None
+
+# Auswahl des aktuellen Mitbewohner
+def select_user():
+    st.sidebar.title("Who are you")
+    if st.session_state["roommates"]:
+        selected_user = st.sidebar.selectbox("Choose your name:", st.session_state["roommates"])
+        st.session_state["selected_user"] = selected_user
+        st.sidebar.write(f"Hi, {selected_user}!")
+    else:
+        st.sidebar.warning("No user was added.")
