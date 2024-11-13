d6e03c4aa16cddf826d7c2dad8a5e99726de31ab
Luciano-Covella <luciano.covella@student.unisg.ch>
2024-11-13 20:53:53 +0100

    CC

--- workplace_lucio.py
+++ workplace_lucio.py
@@ -13,7 +13,7 @@ from recipe_page import recipepage
 st.markdown("""
     <style>
     .stButton>button:hover {
-        background-color: green !important;
+        background-color: red !important;
         color: white !important;
     }
     </style>
